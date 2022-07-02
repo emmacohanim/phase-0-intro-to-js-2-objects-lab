0d85b8392fbbb1d1b5a71163aeb92ddbae7d6236
Emma Cohanim <emmacohanim@gmail.com>
2022-07-02 14:08:57 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "employeeName",
+    streetAddress: "employeeAddress"
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+    //Can I use new employee, or do I need another constant?//
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
